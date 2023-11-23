bf529b68d123ccb3f5bdcf8461f1ccb0ff9c40a7
LauriVoco <lauri.liias@voco.ee>
2023-11-23 15:39:43 +0200

    LAB1 Ülesanne

---
+++ YL1.ps1
@@ -0,0 +1,15 @@
+﻿#Eesnimi kasutajal
+$eesnimi = Read-Host 'Sisesta oma eesnimi'
+
+#Perenimi kasutajal
+$perenimi = Read-Host 'Sisesta oma perenimi'
+
+#Loome kasutajanime kasutaja ees ja perekonnanimest muutes need väiketähtedeks ja keskele lisades punkti.
+$Kasutajanimi = "$($eesnimi.tolower()).$($perenimi.tolower())"
+
+Write-Host "Loodav kasutaja on"$Kasutajanimi
+
+#Anname kasutajale parooli
+$kasutajaparool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+New-LocalUser -Name "$kasutajanimi" -Password $kasutajaparool -FullName "$eesnimi $perenimi" -Description "Local Account"
