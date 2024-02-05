cf1e8f277fc1ab8b130f70b7d7d00acf287e5c92
LauriVoco <Lauri.Liias@voco.ee>
2024-02-05 14:12:00 +0200

    Ülesanne 2

---
+++ YL2.ps1
@@ -0,0 +1,13 @@
+﻿#Skript küsib kasutajalt eesnime ja perekonnanime.
+$eesnimi = Read-Host "Sisesta kasutaja eesnimi"
+$perenimi = Read-Host "Sisesta kasutaja perenimi"
+
+$kasutaja = "$($eesnimi.ToLower()).$($perenimi.ToLower())"
+
+#Eemaldab kasutajapoolt soovitud kasutaja
+Remove-LocalUser -Name $kasutaja -Verbose
+if ($?) {
+    Write-Host "Kasutaja $kasutaja on edukalt kustutatud."
+} else {
+    Write-Host "Kustutamisega tekkis probleem."
+}
